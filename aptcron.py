78e2e857907a29935f20157fdbe61a648a95cec6
Mathias Ertl <mati@er.tl>
2014-04-04 12:28:56 +0200

    immplement check for root

--- aptcron.py
+++ aptcron.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
+import os
+import sys
+
 import apt
 
+if os.getuid() != 0:
+    print("aptcron requires root-privileges to run.", file=sys.stderr)
+    sys.exit(1)
+
+# initialize cache:
 cache=apt.Cache()
-#cache.update()
+cache.update()
+
+# list upgradeable packages
 cache.open(None)
 cache.upgrade()
 for pkg in cache.get_changes():
