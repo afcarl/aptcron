8b5a529ea1d7fa01cad96a03c4f188b6a6d27b84
Mathias Ertl <mati@er.tl>
2014-04-04 12:35:01 +0200

    add argument parser, make updating the index optional

--- aptcron.py
+++ aptcron.py
@@ -2,10 +2,19 @@
 
 from __future__ import print_function
 
+import argparse
 import os
 import sys
 
 import apt
+
+parser = argparse.ArgumentParser(
+    description="List APT updates via cron, optionally installing them.")
+parser.add_argument('--no-update', dest='update', default=True, action='store_false',
+                    help="Do not update package index before listing updates.")
+parser.add_argument('--install', default=False, action='store_true',
+                    help="Install all upgrades as well.")
+args = parser.parse_args()
 
 if os.getuid() != 0:
     print("aptcron requires root-privileges to run.", file=sys.stderr)
@@ -13,7 +22,10 @@ if os.getuid() != 0:
 
 # initialize cache:
 cache=apt.Cache()
-cache.update()
+
+# update the APT cache.
+if args.update:
+    cache.update()
 
 # list upgradeable packages
 cache.open(None)
