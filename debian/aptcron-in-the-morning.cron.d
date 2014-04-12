eeaf5dbf8f8123bb525094a6bcd659eeb50b5885
Mathias Ertl <mati@er.tl>
2014-04-12 15:03:26 +0200

    install random times with --random option,  not with shakey cron jobs

--- debian/aptcron-in-the-morning.cron.d
+++
@@ -1,5 +0,0 @@
-# Run aptcron sometime in the morning (between 3:00 am and 8:00 am)
-SHELL=/bin/bash
-
-# m h dom mon dow user  command
-55 2     * * *   root    echo aptcron | at $(($RANDOM \% 5 + 3)):$(printf \%02d $(($RANDOM \% 60))) 2> /dev/null
--- debian/aptcron-random-time.cron.d
+++
@@ -1,8 +0,0 @@
-# Run aptcron at a random time of day using the "at" command.
-# 
-# NOTE: In the off-chance that the random time is 0:00, aptcron might actually
-#       run the next day.
-SHELL=/bin/bash
-
-# m h dom mon dow user  command
-0 0     * * *   root    echo aptcron | at $(($RANDOM \% 24)):$(printf \%02d $(($RANDOM \% 60))) 2> /dev/null
