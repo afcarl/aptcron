c7ce06d0a2d0643d844584e86c4ba68b46e2c31a
Mathias Ertl <mati@er.tl>
2014-04-12 15:43:41 +0200

    use individual crontabs again

--- debian/aptcron.cron.d
+++
@@ -1,8 +0,0 @@
-# Run aptcron.
-#
-# NOTE: The actual time of execution will depend on the configuration files in
-#       /etc/aptcron.d/: If the random-time is given, it will execute sometime
-#       in that timerange, if not, aptcron will execute right away.
-
-# m h dom mon dow user  command
-0 0     * * *   root    aptcron
