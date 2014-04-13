52dbe19d3dd779a2073abd6af88a31bac564d095
Mathias Ertl <mati@er.tl>
2014-04-13 19:36:32 +0200

    update debian package

--- debian/aptcron-in-the-morning.cron.d
+++ debian/aptcron-in-the-morning.cron.d
@@ -1,6 +1,5 @@
 # Run aptcron sometime in the morning (between 2:00 am and 8:00 am) using the
 # "at" command.
-# The time range is configured with the random-time setting in /etc/aptcron.d/
 
 # m h dom mon dow   user    command
-55 1  * * *         root    aptcron
+55 1  * * *         root    aptcron --random-time 2:00-8:00
--- debian/aptcron-random-time.cron.d
+++ debian/aptcron-random-time.cron.d
@@ -1,5 +1,4 @@
 # Run aptcron at a random time of day using the "at" command.
-# The time range is configured with the random-time setting in /etc/aptcron.d/
 
 # m h dom mon dow   user    command
-0 0   * * *         root    aptcron
+0 0   * * *         root    aptcron --random-time
